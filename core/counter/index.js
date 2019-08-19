26062b3437f0466652ca34a08819c9e7f5fbbec8
Daniel Yo <dnl992004@yahoo.com.au>
2019-08-19 14:08:30 -0700

    renamed to index and updated import/export

---
+++ core/counter/index.js
@@ -0,0 +1,12 @@
+import CounterReducer from './CounterReducer'
+import CounterActions from './CounterAction'
+import CounterView from './CounterView'
+
+console.log("CounterAction ACTIONS")
+console.log(CounterActions)
+
+export {
+  CounterReducer,
+  CounterActions,
+  CounterView
+}
